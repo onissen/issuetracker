4cbd5b7a79a3254bb87a39654bd9d2caf7e8e7b8
Oke <56229919+onissen@users.noreply.github.com>
2022-08-26 20:20:49 +0200

    Zustand

--- components/header.php
+++ components/header.php
@@ -12,7 +12,6 @@
     <link rel="stylesheet" href="<?php echo $SiteURL ?>/css/main.css">
 </head>
 <body>
-    <i class="myoctions myocticons-issue-closed"></i>
     <nav class="navbar navbar-expand-md navbar-dark bg-primary">
         <div class="container-fluid">
             <a class="navbar-brand" href="index.php">
@@ -23,42 +22,9 @@
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                     <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                        <a class="nav-link" aria-current="page" href="index.php">Themenübersicht</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
-    <div class="topic-header container-fluid pt-3">
-        <div class="topic-headline px-3 px-md-4 px-lg-5">
-            <i class="text-muted octicon octicon-repo"></i>
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="http://">onissen</a></li>
-                <li class="breadcrumb-item active" area-current="page"><a href="http://">issuetracker</a></li>
-            </ol>
-            <span class="badge rounded-pill label-muted-outline">Visibility Label</span>
-        </div>
-        <div class="clearfix"></div>
-        <nav class="topic-nav px-3 px-md-4 px-lg-5">
-            <ul class="nav nav-pills">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Active</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
