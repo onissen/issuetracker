57276728c913ad282f394d0cd8baed025f73cc0a
Oke <56229919+onissen@users.noreply.github.com>
2022-09-10 19:58:35 +0200

    Submit hatte Bug, wenn Val schon mal übergeben

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 searchbox = document.querySelector("#search-issues");
-
+URLRequest=location.search;
 searchbox.addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.key === "Enter") {
-        // Hier wird der Query Inhalt zur URL hinzugefügt:
-        location.search += "&"+searchbox.name+"="+searchbox.value;       
+        if (URLRequest != '' || !URLRequest.startsWith('?search=')) {
+            console.log(URLRequest);
+            if (URLRequest.includes("&search")) {
+                old_search = URLRequest.substring(URLRequest.indexOf("&search"));
+                URLRequest = URLRequest.replace(old_search, ''); // ?filter...
+            }
+            URLRequest += '&search='+searchbox.value;
+
+        } else {URLRequest = '?search='+searchbox.value;}
+        location.search = URLRequest;
     }
 });
