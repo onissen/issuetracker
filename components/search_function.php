e461c31c44e2fac744fe4fd467caa60b5d35a993
Oke <56229919+onissen@users.noreply.github.com>
2022-09-11 02:06:31 +0200

    ...

--- components/search_function.php
+++ components/search_function.php
@@ -1,5 +1,5 @@
 <?php
-            
+    //  ... wenn nach Status gefiltert, sonst alle
     if (isset($_REQUEST['filter'])) {
         if ($_REQUEST['filter'] == '' OR $_REQUEST['filter'] == 'open') {$sql_param = "AND status='open' ";}
         if ($_REQUEST['filter'] == 'closed') {$sql_param =  "AND status='closed' ";}
@@ -8,36 +8,43 @@
         $sql_param = "AND status='open'";
     }
 
+    // Wenn Suchbegriffe eingegeben sind
     if (isset($_REQUEST['search'])) {
+        // Suchbegriffe an den Leerzeichen teilen
         $raw_search = explode(' ', $_REQUEST['search']);
-        
+
         foreach ($raw_search as $key => $value) {
+            // Wenn der einzelne Begriff einen bestimmten typ: hat, nimm ihn auseinander und übergebe ihn in PHP Array
             if (strpos($value, ':') != false) {
                 $subs = explode(':', $value);
                 $queries[$key]['type'] = $subs[0];
                 $queries[$key]['q'] = $subs[1];
 
             } else {
+                // Wenn keine anderen Begriffe mit Typ angeben sind, vergebe query
                 if (!isset($queries[$key-1]['type'])) {
                     $queries[$key]['type'] = 'query';
                     $queries[$key]['q'] = $value;
                 } else {
+                    // Füge das abgetrennte, aber zum Begriff gehörende Wort mit Leerzeichen wieder hinzu
                     $queries[$key-1]['q'] .= ' '.$value;
                 }
-                print_r($queries);
-        }   }
+            }
+        }
 
         foreach ($queries as $key => $value) {
             $SearchRow = $queries[$key]['type'];
             $SearchTerm = $queries[$key]['q'];
+            // Wenn kein Typ oder der allgemeine query angegeben ist wird alles durchsucht
             if ($SearchRow == 'query') {
                 $sql_param .= "AND(title LIKE '%$SearchTerm%' OR author LIKE '%$SearchTerm%' OR issue_id LIKE '%$SearchTerm%' OR status LIKE '%$SearchTerm%')";
             }
+            // Sonst wird jeder Parameter automatisch einzeln hinzugefügt
             else {$sql_param .= " AND $SearchRow LIKE '%$SearchTerm%'";}
         }
     }
 
+    // Anweisung zusammen schustern
     $sql_issues = "SELECT issues.* FROM issues WHERE tpid = $topicid $sql_param ORDER BY issue_id DESC";
-    echo $sql_issues;
     $result_issues = $db->query($sql_issues)->fetchAll();
 ?>
--- js/main.js
+++ js/main.js
@@ -16,33 +16,3 @@ searchbox.addEventListener("keyup", function(event) {
         searchIssues();
     }
 });
-
-// SearchInput = document.getElementById('search-issues');
-
-
-// function searchIssues () {
-//     var input, filter, listDiv, itemDiv, td, i, txtValue;
-    
-//     input = document.getElementById('search-issues');
-//     filter = input.value.toLowerCase();
-    
-//     listDiv = document.getElementById('issuelist-list'); // FIXME: Das wäre der alles umschließende div ?!
-//     itemDiv = listDiv.querySelectorAll('issuelist-item'); // FIXME: Wäre jeder weitere DIv Eintrag...
-
-//     for (i = 0; i < itemDiv.length; i++) {
-//         // td = itemDiv[i].getElementsByTagName('td')  [0];
-//         title = itemDiv[i].querySelector('.title-link');
-//         id = itemDiv[i].querySelector('.issuelist-meta.id');
-//         author = itemDiv[i].querySelector('.issuelist-meta.author');
-//         //TODO: HIer noch LAbels, Milestones... 
-        
-//         if (title) {
-//             txtValue = title.textContent || title.innerText;
-//             if (txtValue.toLowerCase().indexOf(filter) > -1) {
-//                 itemDiv[i].style.display = "";
-//             } else {
-//                 itemDiv[i].style.display = "none";
-//             }
-//         }
-//     }
-// }
