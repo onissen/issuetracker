4bb289a0769da7e63036c87bf071d5e6f0887069
Oke <56229919+onissen@users.noreply.github.com>
2022-10-03 20:56:04 +0200

    editing works

--- labels.php
+++ labels.php
@@ -24,6 +24,19 @@
             echo '<script type="text/JavaScript"> location.search = "";</script>';
         }
     }
+
+    if (isset($_GET['edited'])) {
+        $labelid = $_GET['edited'];
+        $name = $_POST['name'];
+        $description = $_POST['description'];
+        $color = $_POST['color'];
+        $sql_edit = "UPDATE labels SET name='$name', description='$description', color='$color' WHERE labelid=$labelid";
+        $stmt_edit = $db->prepare($sql_edit);
+        $stmt_edit->execute();
+        if ($stmt_edit->execute()) {
+            echo '<script type="text/JavaScript"> location.search = "";</script>';
+        }
+    }
 ?>
 
 <div class="container container-xl mt-4">
@@ -71,17 +84,76 @@
         <div class="issuelist-list" id="issuelist-list">
             <?php if ($labelAverage > 0) { ?>
                 <?php foreach ($result_list as $list) { ?>
-                    <div class="issuelist-item d-flex p-3" id="<?php echo $list['labelid']; ?>">
-                        <div class="issuelist-content w-100">
-                            <div class="label-badge badge rounded-pill" style="background-color: <?php echo $list['color'] ?>"><?php echo $list['name'] ?></div>
+                    <div class="issuelist-item p-3" id="<?php echo $list['labelid']; ?>">
+                        <div class="d-flex">
+                            <div class="issuelist-content w-100">
+                                <div class="label-badge badge rounded-pill" id="badge-<?php echo 'color'.$list['labelid'] ?>" style="background-color: <?php echo $list['color'] ?>"><?php echo $list['name'] ?></div>
+                            </div>
+                            <div class="labellist-description js-hide<?php echo $list['labelid'] ?> pe-2 w-100"><?php echo $list['description'] ?></div>
+                            <?php IssueAmount() ?>
+                            <div class="labellist-issues js-hide<?php echo $list['labelid'] ?> w-100">x offene Issues</div>
+                            <div class="labellist-actions text-end d-lg-flex d-none">
+                                <button class="js-hide<?php echo $list['labelid'] ?> ms-3 btn btn-link" data-bs-toggle="collapse" href="#wrapper-<?php echo $list['labelid']; ?>" role="button" aria-expanded="false" aria-controls="wrapper-<?php echo $list['labelid']; ?>" onclick="DisplayMeta('none', <?php echo $list['labelid']; ?>)">Bearbeiten</button>
+                                <button class="ms-3 btn btn-link" name="deleteLabel" type="submit" onclick="toggleDelete(<?php echo $list['labelid'] ?>)">Löschen</button>
+                            </div>
                         </div>
-                        <div class="labellist-description pe-2 w-100"><?php echo $list['description'] ?></div>
-                        <?php IssueAmount() ?>
-                        <div class="labellist-issues w-100">x offene Issues</div>
-                        <div class="labellist-actions text-end d-lg-flex d-none">
-                            <button class="ms-3 btn btn-link">Bearbeiten</button>
-                                <!-- Dann Collapse -->
-                            <button class="ms-3 btn btn-link" name="deleteLabel" type="submit" onclick="toggleDelete(<?php echo $list['labelid'] ?>)">Löschen</button>
+                        <div class="collapse mt-3" id="wrapper-<?php echo $list['labelid']; ?>">
+                            <form action="?edited=<?php echo $list['labelid'] ?>" method="post">
+                                <div class="row">
+                                    <div class="col-md-3 col-12" id="col-name">
+                                        <label for="input-name" class="form-label">Label Name</label>
+                                        <input type="text" name="name" id="input-name" class="form-control-sm form-control" value="<?php echo $list['name'] ?>">
+                                    </div>
+                                    <div class="col-lg-4 col-md-3 col-12" id="col-description">
+                                        <label for="input-description" class="form-label">Beschreibung</label>
+                                        <input type="text" name="description" id="input-description" class="form-control-sm form-control" value="<?php echo $list['description'] ?>">
+                                    </div>
+                                    <div class="col-md-2 col-12" id="col-color">
+                                        <label for="input-color" class="form-label">Color</label>
+                                        <div class="d-flex">
+                                            <button type="button" class="btn btn-sm btn-light text-light me-2" id="button-<?php echo 'color'.$list['labelid'] ?>" style="background-color: <?php echo $list['color'] ?>" onclick="randomBtnColor('<?php echo 'color'.$list['labelid'] ?>')">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+                                                    <path fill-rule="evenodd" d="M8 2.5a5.487 5.487 0 00-4.131 1.869l1.204 1.204A.25.25 0 014.896 6H1.25A.25.25 0 011 5.75V2.104a.25.25 0 01.427-.177l1.38 1.38A7.001 7.001 0 0114.95 7.16a.75.75 0 11-1.49.178A5.501 5.501 0 008 2.5zM1.705 8.005a.75.75 0 01.834.656 5.501 5.501 0 009.592 2.97l-1.204-1.204a.25.25 0 01.177-.427h3.646a.25.25 0 01.25.25v3.646a.25.25 0 01-.427.177l-1.38-1.38A7.001 7.001 0 011.05 8.84a.75.75 0 01.656-.834z"></path>
+                                                </svg>
+                                            </button>
+                                            <div class="position-relative flex-1">
+                                                <input type="text" name="color" id="input-<?php echo 'color'.$list['labelid'] ?>" class="form-control-sm form-control" value="<?php echo $list['color'] ?>" onfocus="toggleColorPicker('<?php echo 'color'.$list['labelid'] ?>')" onkeyup="dynamicBtnColor('<?php echo 'color'.$list['labelid'] ?>')">
+                                                <div class="gh-popover color-popover" id="popover-<?php echo 'color'.$list['labelid'] ?>">
+                                                    <div class="popover-message popover-message-bottom-left p-2 mt-2 shadow-lg">
+                                                        <p>Aus Standardfarben wählen</p>
+                                                        <div class="d-flex justify-content-between mb-2">
+                                                            <button style="background-color: #b60205" onclick="chooseColor('#b60205', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                            <button style="background-color: #D93F0B" onclick="chooseColor('#D93F0B', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                            <button style="background-color: #FBCA04" onclick="chooseColor('#FBCA04', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                            <button style="background-color: #0E8A16" onclick="chooseColor('#0E8A16', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                            <button style="background-color: #006B75" onclick="chooseColor('#006B75', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                            <button style="background-color: #1D76DB" onclick="chooseColor('#1D76DB', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                            <button style="background-color: #0052CC" onclick="chooseColor('#0052CC', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                            <button style="background-color: #5319E7" onclick="chooseColor('#5319E7', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                        </div>
+                                                        <div class="d-flex justify-content-between">
+                                                            <button style="background-color: #E99695" onclick="chooseColor('#E99695', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                            <button style="background-color: #F9D0C4" onclick="chooseColor('#F9D0C4', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                            <button style="background-color: #FEF2C0" onclick="chooseColor('#FEF2C0', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                            <button style="background-color: #C2E0C6" onclick="chooseColor('#C2E0C6', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                            <button style="background-color: #BFDADC" onclick="chooseColor('#BFDADC', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                            <button style="background-color: #C5DEF5" onclick="chooseColor('#C5DEF5', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                            <button style="background-color: #BFD4F2" onclick="chooseColor('#BFD4F2', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                            <button style="background-color: #D4C5F9" onclick="chooseColor('#D4C5F9', '<?php echo 'color'.$list['labelid'] ?>')" class="color-swatch btn-light" type="button"></button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-3 col-md-4 col-12 d-flex" id="col-buttons">
+                                        <div>
+                                            <button type="button" class="me-1 btn btn-sm btn-gh" data-bs-toggle="collapse" href="#wrapper-<?php echo $list['labelid']; ?>" role="button" aria-expanded="true" aria-controls="wrapper-<?php echo $list['labelid']; ?>" onclick="DisplayMeta('block', '<?php echo $list['labelid']; ?>')">Schließen</button>
+                                            <button type="submit" class="btn btn-sm btn-success">Änderungen speichern</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 <?php } ?>
