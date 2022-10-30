6272461080e6f0f1d6cede9141783ccc50b12046
Oke <56229919+onissen@users.noreply.github.com>
2022-10-30 22:43:27 +0100

    Search() in den Labels läuft auch

--- labels.php
+++ labels.php
@@ -74,7 +74,7 @@
                         <path fill-rule="evenodd" d="M11.5 7a4.499 4.499 0 11-8.998 0A4.499 4.499 0 0111.5 7zm-.82 4.74a6 6 0 111.06-1.06l3.04 3.04a.75.75 0 11-1.06 1.06l-3.04-3.04z"></path>
                     </svg>
                 </span>
-                <input type="text" name="search" id="searchbox" class="form-control form-control-sm" value="<?php if (isset($_REQUEST['search'])) {echo $_REQUEST['search'];}?>" autocomplete="off" placeholder="Alle Labels durchsuchen">
+                <input type="text" name="search" id="searchbox" class="form-control form-control-sm" value="<?php if (isset($_REQUEST['search'])) {echo $_REQUEST['search'];}?>" autocomplete="off" placeholder="Alle Labels durchsuchen" onkeyup="Search(event)">
             </div>
         </div>
         <div class="col-md-4 search-nav text-end">
