57c4addfc340155de9422328293216a32eb53b49
Oke <56229919+onissen@users.noreply.github.com>
2022-11-02 19:41:05 +0100

    Bug on Issue_average

--- issues.php
+++ issues.php
@@ -10,8 +10,8 @@
 <?php
     $topicid = $info['tpid'];
 
-    $sql_averages = "SELECT (SELECT COUNT(id) FROM issues WHERE status = 'open') as issues_open, 
-    (SELECT COUNT(id) FROM issues WHERE status = 'closed' OR status = 'notplanned') as issues_closed, 
+    $sql_averages = "SELECT (SELECT COUNT(id) FROM issues WHERE status = 'open' AND tpid = $topicid) as issues_open, 
+    (SELECT COUNT(id) FROM issues WHERE (status = 'closed' OR status = 'notplanned') AND tid = $topicid) as issues_closed, 
     (SELECT COUNT(labelid) FROM labels WHERE topicid = $topicid) as average_labels";
 
     $query_averages = $db->prepare($sql_averages);
@@ -134,13 +134,13 @@
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
                             <path d="M8 9.5a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path><path fill-rule="evenodd" d="M8 0a8 8 0 100 16A8 8 0 008 0zM1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0z"></path>
                         </svg>
-                        <?php echo $averages['issues_open'].' Offen'; ?>
+                        <?php if ($averages['issues_open'] > 0) {echo $averages['issues_open'].' Offen';} else {echo '0 Offen';} ?>
                     </a>
                     <a href="?filter=closed" class="btn-link <?php if($_REQUEST['filter'] == 'closed'){echo 'selected';} ?>">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
                             <path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path>
                         </svg>
-                        <?php echo $averages['issues_closed'].' Erledigt'; ?>
+                        <?php if ($averages['issues_closed'] > 0) {echo $averages['issues_closed'].' Erledigt';} else {echo '0 Erledigt';} ?>
                     </a>
                 </div>
                 <div class="col-md-9 d-flex no-wrap justify-content-between justify-content-sm-start justify-content-lg-end action-menu">
